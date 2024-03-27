5ff067854fa9f22f915fe4d50c48834d273e3343
mstent <mlstenton@gmail.com>
2024-03-27 11:06:36 +0000

    fixed broken link

--- app/(LoginSignUp)/(app)/AR.js
+++ app/(LoginSignUp)/(app)/AR.js
@@ -4,7 +4,7 @@ import { router } from "expo-router";
 export default function ARArea() {
 
     const backToMapHandler = () => {
-        router.replace('/MapView')
+        router.replace('/MapScreen')
     }
 
     return (
