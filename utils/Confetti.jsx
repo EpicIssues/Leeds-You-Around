c87bc32cfed483be2fcd92dd3ec092ff8327815d
mundocalrisian <greg.munden@gmail.com>
2024-04-04 13:20:10 +0100

    add confetti before rewards screen

---
+++ utils/Confetti.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import LottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function ConfettiAnimation() {
+    const navigation = useNavigation()
+
+    const animationRef = useRef(LottieView);
+
+    useEffect(() => {
+    animationRef.current?.play();
+
+    // Or set a specific startFrame and endFrame with:
+    // animationRef.current?.play(0);
+    }, []);
+
+    setTimeout(() => {
+        navigation.navigate("RewardsScreen")
+    }, 8500)
+
+    return (
+        <View>
+            <LottieView
+                ref={animationRef}
+                source={require("../assets/confetti.json")}
+                style={{width: "100%", height: "100%"}}
+                loop={false}
+                resizeMode="cover"
+            />
+        </View>
+
+    );
+}
